102f29dcbfd02c159475b2a2f620675cedc2ab74
hosch3n <29953808+hosch3n@users.noreply.github.com>
2020-08-06 16:48:38 +0800

    replace sougoupinyin_to_googlepinyin

--- 2.install_app.sh
+++ 2.install_app.sh
@@ -20,11 +20,12 @@ apt install -y foremost volatility
 gem install evil-winrm
 
 # Common
-apt install -y keepass2 fcitx audacity ffmpeg obs-studio aria2 mesa-utils
+apt install -y keepass2 fcitx fcitx-pinyin audacity ffmpeg obs-studio aria2 \
+ mesa-utils
 
 # SouGouPinyin
-apt install -y fcitx-libs qml-module-qtquick2 libqt5quickwidgets5
-dpkg -i sogouimebs_2.0.0.38+0428.1_amd64.deb
+#apt install -y fcitx-libs qml-module-qtquick2 libqt5quickwidgets5
+#dpkg -i sogouimebs_2.0.0.38+0428.1_amd64.deb
 
 pip install pwntools
 pip3 install docker-compose pyjwt pycryptodome py2exe threadpool you-get
