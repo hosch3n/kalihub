210c75b7d897a390f8f6d0af72e9a9ea11dbc55c
hosch3n <29953808+hosch3n@users.noreply.github.com>
2020-08-24 10:55:54 +0800

    add libssl-dev&zsteg&multihashes

--- 2.install_app.sh
+++ 2.install_app.sh
@@ -8,16 +8,17 @@ apt install -y virtualbox docker-ce wine wine64
 
 # Development
 apt install -y php7.4-fpm tomcat9 redis golang jython
+#npm install multihashes
 
 # Dependency
-apt install -y python-pip python3-pip python3-venv composer
+apt install -y python-pip python3-pip python3-venv composer libssl-dev
 
 # Debug
 apt install -y php-xdebug gdb strace android-tools-adb fastboot
 
 # Security
 apt install -y foremost volatility apktool
-gem install evil-winrm
+gem install evil-winrm zsteg
 
 # Common
 apt install -y keepass2 fcitx fcitx-googlepinyin audacity ffmpeg obs-studio \
@@ -27,7 +28,7 @@ apt install -y keepass2 fcitx fcitx-googlepinyin audacity ffmpeg obs-studio \
 #apt install -y fcitx-libs qml-module-qtquick2 libqt5quickwidgets5
 #dpkg -i sogouimebs_2.0.0.38+0428.1_amd64.deb
 
-pip install pwntools
+pip install pwntools zio
 pip3 install docker-compose pyjwt pycryptodome py2exe threadpool you-get
 
 # Proxy
